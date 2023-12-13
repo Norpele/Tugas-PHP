c3e9aaec5ab5ccc8b785af48844fe45704d1e26a
Norpele <123357969+Norpele@users.noreply.github.com>
2023-12-13 14:52:59 +0700

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,317 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "sekolah";
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+if (!$koneksi) { //cek koneksi
+    die("Tidak bisa terkoneksi ke database");
+}
+$username = "";
+$password = "";
+$status = "";
+
+if (isset($_GET['op'])) {
+    $op = $_GET['op'];
+} else {
+    $op = "";
+}
+if ($op == 'delete') {
+    $id = $_GET['id'];
+    $sql1 = "delete from admin where id = '$id'";
+    $q1 = mysqli_query($koneksi, $sql1);
+    if ($q1) {
+        $sukses = "Berhasil hapus data";
+    } else {
+        $error = "Gagal melakukan delete data";
+    }
+}
+if ($op == 'edit') {
+    $id = $_GET['id'];
+    $sql1 = "select * from admin where id = '$id'";
+    $q1 = mysqli_query($koneksi, $sql1);
+    $r1 = mysqli_fetch_array($q1);
+    $username = $r1['username'];
+    $password = $r1['password'];
+    $status = $r1['status'];
+
+    if ($username == '') {
+        $error = "Data tidak ditemukan";
+    }
+}
+if (isset($_POST['simpan'])) { //untuk create
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $status = $_POST['status'];
+
+    if ($username && $password && $status) {
+        if ($op == 'edit') { //untuk update
+            $sql1 = "update admin set username = '$username',password ='$password',status = '$status' where id = '$id'";
+            $q1 = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses = "Data berhasil diupdate";
+            } else {
+                $error = "Data gagal diupdate";
+            }
+        } else { //untuk insert
+            $sql1 = "insert into admin(username,password,status) values ('$username','$password','$status')";
+            $q1 = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses = "Berhasil memasukkan data baru";
+            } else {
+                $error = "Gagal memasukkan data";
+            }
+        }
+    } else {
+        $error = "Masukan Data";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
+    <style>
+        .mx-auto {
+            width: 800px;
+        }
+
+        .card {
+            margin-top: 10px;
+        }
+        header {
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+            height: auto;
+            box-shadow: 0px 0px 8px 0px black;
+            background-color: white;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+        }
+
+        body {
+            margin-bottom: 50px;
+        }
+
+        .judul {
+            width: 50%;
+            margin: 10px;
+            height: 50px;
+            text-align: center;
+        }
+
+        h1 {
+            font-family: sans-serif;
+            font-weight: 50px;
+            text-decoration-color: blue;
+        }
+
+
+        .dropbtn {
+            margin-top: 20px;
+            margin-right: 160px;
+            padding-top: 5px;
+        }
+
+        button {
+            background: none;
+            border: none;
+            padding: 0;
+            font: inherit;
+            cursor: pointer;
+            outline: inherit;
+        }
+
+        .dropdown {
+            position: relative;
+            display: inline-block;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+        }
+
+        .dropdown-content a:hover {
+            background-color: #f1f1f1;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+
+        .container {
+            width: 100%;
+            height: auto;
+        }
+
+        .mx-auto {
+            width: 800px;
+            margin-top: 125px;
+            margin-bottom: 125px;
+        }
+
+        .card {
+            margin-top: 10px;
+        }
+
+        .card-footer {
+            background-color: black;
+            height: 100px;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        footer p {
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+    <header>
+        <div class="judul">
+            <h1>Sistem informasi akademik</h1>
+        </div>
+        <div class="dropdown">
+            <button onclick="myfunction()" class="dropbtn">Menu</button>
+            <div id="menudropdown" class="dropdown-content">
+                <a href="index.php">Home</a>
+                <a href="admin.php">Admin</a>
+                <a href="walimahasiswa.php">Wali Mahasiwa</a>
+                <a href="index.php">Mahasiswa</a>
+            </div>
+    </header>
+    </header>
+    <div class="mx-auto">
+        <!-- untuk memasukkan data -->
+        <div class="card">
+            <div class="card-header">
+                Create / Edit Data
+            </div>
+            <div class="card-body">
+                <form action="" method="POST">
+                    <div class="mb-3 row">
+                        <label for="username" class="col-sm-2 col-form-label">Username</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="username" name="username" value="<?php echo $username ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="password" class="col-sm-2 col-form-label">password</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" id="password" name="password"
+                                value="<?php echo $password ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="status" class="col-sm-2 col-form-label">status</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="status" name="status"
+                                value="<?php echo $status ?>">
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <!-- untuk mengeluarkan data -->
+        <div class="card">
+            <div class="card-header text-white bg-secondary">
+                Data Admin
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Password</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql2 = "select * from admin order by id desc";
+                        $q2 = mysqli_query($koneksi, $sql2);
+                        $urut = 1;
+                        while ($r2 = mysqli_fetch_array($q2)) {
+                            $id = $r2['id'];
+                            $username = $r2['username'];
+                            $password = $r2['password'];
+                            $status = $r2['status'];
+
+                            ?>
+                            <tr>
+                                <th scope="row">
+                                    <?php echo $urut++ ?>
+                                </th>
+                                <td scope="row">
+                                    <?php echo $username ?>
+                                </td>
+                                <td scope="row">
+                                    <?php echo $password ?>
+                                </td>
+                                <td scope="row">
+                                    <?php echo $status ?>
+                                </td>
+                                <td scope="row">
+                                    <a href="index.php?op=edit&id=<?php echo $id ?>"><button type="button"
+                                            class="btn btn-warning">Edit</button></a>
+                                    <a href="index.php?op=delete&id=<?php echo $id ?>"
+                                        onclick="return confirm('Yakin mau delete data?')"><button type="button"
+                                            class="btn btn-danger">Delete</button></a>
+                                </td>
+                            </tr>
+                            <?php
+                        }
+                        ?>
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+<footer>
+    <div class="card-footer" id="footer">
+        <p>&copy; 2023 Thomas Christian Kuntolukito</p>
+    </div>
+</footer>
+
+</html>
